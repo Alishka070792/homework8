531791d6b05d991a7760d3a5da00c203893f1adc
Alina <gazievaalina070792@gmail.com>
2026-09-24 17:53:39 +0300

    init project

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    // Задача 1.1
+        System.out.println("Task 1.1");
+        int[] firstArray = new int[3];
+        firstArray[0] = 1;
+        firstArray[1] = 2;
+        firstArray[2] = 3;
+
+        // Задача 1.2
+        System.out.println("Task 1.2");
+        double [] secondArray = {1.57, 7.654, 9.986};
+
+        // Задача 1.3
+        System.out.println("Task 1.3");
+        String [] thirdArray = new String[] {"winter", "spring", "summer", "autumn"};
+        System.out.println();
+
+    // Задача 2
+        System.out.println("Task 2");
+        for (int i = 0; i < firstArray.length; i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(firstArray[i]);
+        }
+        System.out.println();
+
+
+    }
+}
+
