b31e4e2785ecfad1e6b80bff3764e928392b94c5
Alina <gazievaalina070792@gmail.com>
2026-09-24 18:11:13 +0300

    task 1 is done

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-
-    // Задача 1.1
+        // Задача 1.1
         System.out.println("Task 1.1");
         int[] firstArray = new int[3];
         firstArray[0] = 1;
@@ -14,20 +13,28 @@ public class Main {
 
         // Задача 1.2
         System.out.println("Task 1.2");
-        double [] secondArray = {1.57, 7.654, 9.986};
+        double[] secondArray = {1.57, 7.654, 9.986};
 
         // Задача 1.3
         System.out.println("Task 1.3");
-        String [] thirdArray = new String[] {"winter", "spring", "summer", "autumn"};
+        String[] thirdArray = new String[]{"winter", "spring", "summer", "autumn"};
         System.out.println();
 
-    // Задача 2
+        // Задача 2
         System.out.println("Task 2");
         for (int i = 0; i < firstArray.length; i++) {
             if (i > 0) {
                 System.out.print(", ");
             }
             System.out.print(firstArray[i]);
+        }
+        System.out.println();
+
+        for (int a = 0; a < secondArray.length; a++) {
+            if (a > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(secondArray[a]);
         }
         System.out.println();
 
